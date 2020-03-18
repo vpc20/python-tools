3537e0218de6aca19056f92b71f89fce681922cb
vpc <cuevasvp@gmail.com>
2020-03-18 16:59:54 +0800

    move file on dir up then remove dir

--- move_one_dir_up.py
+++ move_one_dir_up.py
@@ -1,11 +1,13 @@
 import os
 import shutil
 
-for dirpath, dirnames, filenames in os.walk(r"E:\The Handmaid's Tale"):
+for dirpath, dirnames, filenames in os.walk(r"E:\Movies"):
     for dirname in dirnames:
         for dirpath1, dirnames1, filenames1 in os.walk(os.path.join(dirpath, dirname)):
             for filename in filenames1:
                 full_filename = os.path.join(dirpath1, filename)
                 new_name = os.path.join(dirpath, filename)
-                shutil.copyfile(full_filename, new_name)
-                print(f'Copying {filename}...')
+                print(f'Moving {filename}...')
+                shutil.move(full_filename, new_name)  # move one dir up
+                print(f'Remove dir {dirpath1}...')
+                os.rmdir(dirpath1)  # remove dir
