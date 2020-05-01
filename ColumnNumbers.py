d4207fa8297efd8a630db714b030a0e941a8986b
vpc <cuevasvp@gmail.com>
2020-05-02 07:07:49 +0800

    print column markers

--- ColumnNumbers.py
+++ ColumnNumbers.py
@@ -8,9 +8,28 @@ def print_0_to_9(ntimes, nspaces):
     print('')
 
 
-if __name__ == '__main__':
-    print_0_to_9(1, 9)
-    print_0_to_9(10, 0)
+def print_col_markers(n):
+    for i in range(n // 10 + 1):
+        print(f'{i}{" " * (10 - len(str(i)))}', end='')
+    print('')
+    for i in range(n + 1):
+        if i == 0 or i % 10 == 0:
+            print('0', end='')
+        elif i % 5 == 0:
+            print('+', end='')
+        else:
+            print('.', end='')
+    print('')
 
-    print_0_to_9(1, 39)
-    print_0_to_9(10, 3)
+
+if __name__ == '__main__':
+    # print_0_to_9(1, 9)
+    # print_0_to_9(10, 0)
+    #
+    # print_0_to_9(1, 39)
+    # print_0_to_9(10, 3)
+    #
+    # print_0_to_9(1, 9)
+    # print_0_to_9(10, 0)
+
+    print_col_markers(132)
