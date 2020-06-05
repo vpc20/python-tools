0a829b6426c020d7ff70dc1823a708f56d0c9c75
vpc <cuevasvp@gmail.com>
2020-06-05 13:37:49 +0800

    mods

--- ColumnNumbers.py
+++ ColumnNumbers.py
@@ -8,6 +8,16 @@ def print_0_to_9(ntimes, nspaces):
     print('')
 
 
+def print_col_numbers(n):
+    for i in range(0, n, 10):
+        tens = i // 10
+        print(f'{tens}{" " * (39 - (len(str(tens)) - 1))}', end='')
+    print('')
+    for i in range(n):
+        print(f'{i % 10}   ', end='')
+    print('')
+
+
 def print_col_markers(n):
     for i in range(n // 10 + 1):
         print(f'{i}{" " * (10 - len(str(i)))}', end='')
@@ -33,3 +43,5 @@ if __name__ == '__main__':
     # print_0_to_9(10, 0)
 
     print_col_markers(132)
+    print('')
+    print_col_numbers(133)
--- interval_bars.py
+++ interval_bars.py
@@ -1,4 +1,4 @@
-from ColumnNumbers import print_0_to_9
+from ColumnNumbers import print_col_numbers
 
 
 def interval_bars(intvs):
@@ -7,9 +7,20 @@ def interval_bars(intvs):
         print('|' + '-' * ((ftime - stime) * 4 - 1) + '|')
 
 
+def interval_bars_same_line(intvs):
+    prev_ftime = 0
+    for stime, ftime in intvs:
+        if prev_ftime == 0:
+            print(' ' * stime * 4, end='')
+        else:
+            print(' ' * ((stime - prev_ftime) * 4 - 1), end='')
+        print('|' + '-' * ((ftime - stime) * 4 - 1) + '|', end='')
+        prev_ftime = ftime
+    print('')
+
+
 if __name__ == '__main__':
-    print_0_to_9(1, 39)
-    print_0_to_9(4, 3)
+    print_col_numbers(101)
     # interval_bars([(0, 5), (3, 6), (6, 8)])
     # interval_bars([(1, 4), (3, 5), (0, 6), (5, 7), (3, 9), (5, 9), (6, 10), (8, 11), (8, 12), (2, 14), (12, 16)])
     # interval_bars([(1, 4), (5, 7), (8, 11), (12, 16)])
@@ -19,14 +30,8 @@ if __name__ == '__main__':
     #             |-----------|
     #                         |-------|
 
-    intvs = [(5, 11),
-             (5, 7),
-             (1, 3),
-             (1, 7),
-             (1, 3),
-             (9, 12),
-             (9, 12),
-             (11, 15),
-             (12, 15),
-             (12, 15)]
-    interval_bars(intvs)
+    # intvs1 = [(5, 11), (5, 7), (1, 3), (1, 7), (1, 3), (9, 12), (9, 12), (11, 15), (12, 15), (12, 15)]
+    intvs1 = [[3, 5], [9, 20]]
+    interval_bars_same_line(intvs1)
+    intvs2 = [[4, 5], [7, 10], [11, 12], [14, 15], [16, 20]]
+    interval_bars_same_line(intvs2)
