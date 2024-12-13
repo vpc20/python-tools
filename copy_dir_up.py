720cd399bf116196a98c32729358c97125ec3bf1
vpc20 <cuevasvp@yahoo.com>
2024-12-13 09:25:45 +0800

    revision

--- copy_dir_up.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-path = r"C:\Users\vpc\Downloads\network-penetration-testing-python-kali-linux"
-for dirpath, dirnames, filenames in os.walk(path):
-    for filename in filenames:
-        if filename.endswith('text_to_matrix.py'):
-            shutil.copy(os.path.join(dirpath, filename), os.path.join(path, filename))
