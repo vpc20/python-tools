73d476c28549e2aad0e5d3fee08dc8a45bf00b6f
vpc20 <cuevasvp@yahoo.com>
2024-12-15 20:38:05 +0800

    revision

--- matrix_transformation.py
+++ matrix_transformation.py
@@ -2,6 +2,22 @@ def transpose(matrix):
     return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
 
 
+# def rotate(matrix):
+#     result = []
+#     nrows = len(matrix)
+#     ncols = len(matrix[0])
+#
+#     for i in range(ncols):
+#         result.append([])
+#         for j in range(nrows - 1, -1, -1):
+#             result[-1].append(matrix[j][i])
+#     return result
+
+
+def rotate(matrix):  # clockwise
+    return [[matrix[j][i] for j in range(len(matrix) - 1, -1, -1)] for i in range(len(matrix[0]))]
+
+
 if __name__ == '__main__':
 
     m = [[1, 2, 3],
@@ -11,6 +27,10 @@ if __name__ == '__main__':
         print(e)
     print()
 
-    m1 = transpose(m)
+    # m1 = transpose(m)
+    # for e in m1:
+    #     print(e)
+
+    m1 = rotate(m)
     for e in m1:
         print(e)
