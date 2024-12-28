7ae1389cb29be6abb5bcda90dd65af0c0d250a5f
vpc20 <cuevasvp@yahoo.com>
2024-12-29 06:17:03 +0800

    revision

--- matrix_transformation.py
+++ matrix_transformation.py
@@ -1,38 +1,57 @@
-def transpose(matrix):
-    nrows = len(matrix)
-    ncols = len(matrix[0])
-    return [[matrix[j][i] for j in range(nrows)] for i in range(ncols)]
-
-
-# def rotate(matrix):
-#     result = []
+# def transpose(matrix):
 #     nrows = len(matrix)
 #     ncols = len(matrix[0])
-#
-#     for i in range(ncols):
-#         result.append([])
-#         for j in range(nrows - 1, -1, -1):
-#             result[-1].append(matrix[j][i])
-#     return result
+#     return [[matrix[j][i] for j in range(nrows)] for i in range(ncols)]
 
 
-def rotate(matrix):  # clockwise
-    nrows = len(matrix)
-    ncols = len(matrix[0])
-    return [[matrix[j][i] for j in range(nrows - 1, -1, -1)] for i in range(ncols)]
+def transpose(matrix):  # much faster than function above
+    return list(zip(*matrix))
+
+
+# def rotate_clockwise(matrix):
+#     nrows = len(matrix)
+#     ncols = len(matrix[0])
+#     return [[matrix[j][i] for j in range(nrows - 1, -1, -1)] for i in range(ncols)]
+
+
+def rotate_clockwise(matrix):
+    # Transpose the matrix
+    transposed_matrix = [list(row) for row in zip(*matrix)]
+    # Reverse each row to get the rotated matrix (flip vertically)
+    rotated_matrix = [row[::-1] for row in transposed_matrix]
+
+    return rotated_matrix
+
+
+def rotate_counter_clockwise(matrix):
+    # Flip the matrix vertically
+    flipped_vertival_matrix = [row[::-1] for row in matrix]
+
+    # Transpose to get the rotated matrix
+    rotated_matrix = list(zip(*flipped_vertival_matrix))
+
+    return rotated_matrix
+
+
+# def flip_vertical(matrix):
+#     nrows = len(matrix)
+#     ncols = len(matrix[0])
+#     return [[matrix[i][j] for j in range(ncols - 1, -1, -1)] for i in range(nrows)]
 
 
 def flip_vertical(matrix):
-    nrows = len(matrix)
-    ncols = len(matrix[0])
-    return [[matrix[i][j] for j in range(ncols - 1, -1, -1)] for i in range(nrows)]
+    return [row[::-1] for row in matrix]
+
+
+# def flip_horizontal(matrix):
+#     nrows = len(matrix)
+#     ncols = len(matrix[0])
+#     # x = [[matrix[i][j] for j in range(ncols)] for i in range(nrows - 1, -1, -1)]
+#     return [[matrix[i][j] for j in range(ncols)] for i in range(nrows - 1, -1, -1)]
 
 
 def flip_horizontal(matrix):
-    nrows = len(matrix)
-    ncols = len(matrix[0])
-    # x = [[matrix[i][j] for j in range(ncols)] for i in range(nrows - 1, -1, -1)]
-    return [[matrix[i][j] for j in range(ncols)] for i in range(nrows - 1, -1, -1)]
+    return [row for row in matrix[::-1]]
 
 
 if __name__ == '__main__':
@@ -48,14 +67,25 @@ if __name__ == '__main__':
     # for e in m1:
     #     print(e)
 
-    # m1 = rotate(m)
-    # for e in m1:
-    #     print(e)
+    m1 = rotate_clockwise(m)
+    for e in m1:
+        print(e)
+    print()
+
+    m1 = rotate_counter_clockwise(m)
+    for e in m1:
+        print(e)
+    print()
 
     # m1 = flip_vertical(m)
     # for e in m1:
     #     print(e)
 
-    m1 = flip_horizontal(m)
+    # m1 = flip_horizontal(m)
+    # for e in m1:
+    #     print(e)
+
+    m1 = transpose(m)
     for e in m1:
         print(e)
+    print()
